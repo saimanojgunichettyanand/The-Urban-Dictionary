ebffe9b5148406f31316d6af649c501f5c7467f2
saimanojgunichettyanand <49741349+saimanojgunichettyanand@users.noreply.github.com>
2020-05-16 23:16:05 -0400

    this commit includes the following : *UI developed *Animator Added

--- app/src/main/java/com/example/theurbandictionary/MainActivity.kt
+++ app/src/main/java/com/example/theurbandictionary/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.theurbandictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_search.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this,R.anim.blinkinganimation)
+          btn_search.startAnimation(animation)
+        }
     }
+
 }
